23af0e29efbd2d1b555087c61f158cbc2a858090
iram-emir <158573737+Iram-emir@users.noreply.github.com>
2024-05-26 19:31:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+
 //your JS code here. If required.
+document.getElementById('subm').addEventListener('click',()=>{
+	let a = document.getElementById('name1').value
+	let b = document.getElementById('name2').value
+	let c = document.getElementById('name3').value
+	let d = document.getElementById('name4').value
+
+	alert(`First Name:${a}   Last Name:${b}   Phone Number:${c}   Email ID:${d}`)
+})
